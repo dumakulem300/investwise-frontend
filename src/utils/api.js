4a219dd8a384a8d5abd823a932072524caece782
lemgithub <lemuelqcolongon1988@gmail.com>
2026-05-29 19:35:01 +0800

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { auth } from '../firebase';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'https://investwise-backend-575k.onrender.com',
 });
 
 // Request interceptor to add Firebase token
